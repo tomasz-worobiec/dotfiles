7f3485f46dba1ebc7fc6d2fb171176c5506690ad
tomasz-worobiec <tomasz.worobiec@protonmail.com>
2025-07-12 12:42:05 +0000

    greetd: Add config

---
+++ system/greetd.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  ...
+}:
+{
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      user = "tom";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    pkgs.greetd.tuigreet
+  ];
+}
