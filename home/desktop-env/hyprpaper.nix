82914277c7b3b8df201dfb74088c1507c92ce552
Tomasz Worobiec <tomasz.worobiec@protonmail.com>
2025-10-02 21:33:30 +0200

    hyprpaper: Update wallpaper url

--- home/desktop-env/hyprpaper.nix
+++ home/desktop-env/hyprpaper.nix
@@ -3,7 +3,7 @@
 }:
 let
   wallpaper = builtins.fetchurl {
-    url = "https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/nixos.png";
+    url = "https://gruvbox-wallpapers.pages.dev/wallpapers/brands/nixos.png";
     sha256 = "sha256:0s22k5nw3kli9h7h6wb98lrpp3zp0n8zc5bfvyg56rdr57ykk11k";
   };
 in
