7aebc36b25fc242b2f2e0ee0abf13f6096961b9d
Tomasz Worobiec <tomasz.worobiec@protonmail.com>
2026-03-28 10:30:59 +0100

    hyprpaper: Use wallpaper from private resources

--- home/desktop-env/hyprpaper.nix
+++ home/desktop-env/hyprpaper.nix
@@ -1,21 +1,16 @@
 {
+  resources,
   ...
 }:
-let
-  wallpaper = builtins.fetchurl {
-    url = "https://gruvbox-wallpapers.pages.dev/wallpapers/brands/nixos.png";
-    sha256 = "sha256:0s22k5nw3kli9h7h6wb98lrpp3zp0n8zc5bfvyg56rdr57ykk11k";
-  };
-in
 {
   services.hyprpaper = {
     enable = true;
     settings = {
       preload = [
-        "${wallpaper}"
+        "${resources.wallpaper}"
       ];
       wallpaper = [
-        ",${wallpaper}"
+        ",${resources.wallpaper}"
       ];
     };
   };
